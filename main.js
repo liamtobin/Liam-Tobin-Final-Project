f7a1dc3f2e55508adef59d900c669a15d057d501
Liam Tobin <liamtobin@Liams-MacBook-Air.local>
2020-07-17 13:20:49 -0400

    MVP of project complete, mostly responsive, all sections exist, some refactoring rqd

--- main.js
+++ main.js
@@ -1,3 +1,64 @@
-console.log(`the js file is linked`);
-console.log(`ahar matey`);
-let hay = 8;
+function releaseTheSideMenuBalrog() {
+    document.getElementById('modal-menu').classList.add('show-menu');
+};
+
+function leaveNowAndNeverComeBack() {
+    document.getElementById('modal-menu').classList.remove('show-menu');
+};
+
+document.getElementById('menu-trigger').addEventListener('click', releaseTheSideMenuBalrog);
+document.getElementById('menu-close').addEventListener('click', leaveNowAndNeverComeBack);
+
+
+window.onscroll = function() {scrollFunTimes()};
+
+function scrollFunTimes() {
+  if (document.body.scrollTop > 34 || document.documentElement.scrollTop > 34) {
+    document.getElementById('header-desk').classList.add('fixed', 'tranzisheewowow');
+  }
+};
+// window.onscroll = function() {scrollUpFunTimes()};
+
+// function scrollUpFunTimes() {
+//   if (document.body.scrollTop < 35 || document.documentElement.scrollTop < 35) {
+//     document.getElementById('header-desk').classList.remove('fixed', 'tranzisheewowow');
+//   }
+// };
+
+
+const form = document.querySelector('form');
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messages = document.getElementById("messages");
+
+const formUrl = "https://formspree.io/mnqgogdo";
+
+async function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+    const formData = {
+        name: nameInput.value,
+        email: emailInput.value
+    }
+
+    try {
+        messages.innerHTML = "";
+
+        const options = {
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+            method: 'POST',
+        }
+        const response  = await fetch(formUrl, options)
+
+        if (response.ok) {
+            messages.innerHTML = '<span class="success">Thanks for your submission!</span>'
+        } else {
+            throw new Error();
+        }
+    } catch {
+        messages.innerHTML = '<span class="error">Something went wrong. Try again.</span>'
+    }
+};
+
+form.addEventListener('submit', handleFormSubmit);
